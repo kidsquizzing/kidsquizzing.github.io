a507b8b44fce1ead9d26c94cdf289d241d8c4488
kidsquizzing <kidsquizzing@mcncd.org>
2023-10-12 09:02:59 -0500

    test files 4

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'testApp'`, () => {
+  it(`should have as title 'quizzingAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('testApp');
+    expect(app.title).toEqual('quizzingAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('testApp app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('quizzingAngular app is running!');
   });
 });
